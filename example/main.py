022d24c2ca3ec8671242c2753d7f22c225c1a681
Taiki Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2024-11-30 18:15:02 +0900

    👌 IMPROVE: example

--- example/main.py
+++ example/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from mbvh import *
 
@@ -18,12 +19,58 @@ def kinematics(bvh, frame_index):
   
   return frame_list
 
+def plot_frame(bvh, frame_index):
+  frame_list = kinematics(bvh, frame_index)
+
+  fig = plt.figure()
+  ax = fig.add_subplot(111, projection='3d')
+
+  for node in bvh.node_list:
+    # print(frame_list[node.id])
+    if node.parent:
+        p_id = node.parent.id
+        ax.plot(
+            [frame_list[node.id][0, 3], frame_list[p_id][0, 3]],
+            [frame_list[node.id][1, 3], frame_list[p_id][1, 3]],
+            [frame_list[node.id][2, 3], frame_list[p_id][2, 3]],
+            color="black"
+        )
+  
+  # 軸の設定
+  ax.set_xlabel('X')
+  ax.set_ylabel('Y')
+  ax.set_zlabel('Z')
+  ax.set_xlim([-1.5, 1.5])
+  ax.set_ylim([0, 3])
+  ax.set_zlim([0, 3])
+  
+  # 各行列を描画
+  for i, frame in enumerate(frame_list):
+    origin = frame[:3, 3]  # 平行移動部分 (x, y, z)
+    x_axis = frame[:3, 0]  # x軸
+    y_axis = frame[:3, 1]  # y軸
+    z_axis = frame[:3, 2]  # z軸
+    
+    # 原点を描画
+    ax.scatter(*origin, label=f'Frame {i}')
+    
+    # 軸を描画
+    ax.quiver(*origin, *x_axis, color='r', length=0.1, normalize=True, label=f'X{i}')
+    ax.quiver(*origin, *y_axis, color='g', length=0.1, normalize=True, label=f'Y{i}')
+    ax.quiver(*origin, *z_axis, color='b', length=0.1, normalize=True, label=f'Z{i}')
+  
+  # plt.legend()
+  plt.show()
+
 def main():
-  with open('./test.bvh', 'r') as f:
-    bvh = Bvh.read_bvh(f)
-      
-  frame_list = kinematics(bvh, 0)
-  print(frame_list)
+  with open('./test.bvh', 'r') as file:
+    file_content = file.read()
+  bvh = Bvh.parse_bvh(file_content)
+
+  bvh.show_node_tree()
+  bvh.show_frame(0)
+
+  plot_frame(bvh, 0)
 
 if __name__ == "__main__":
     main()
